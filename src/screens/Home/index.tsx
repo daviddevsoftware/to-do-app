b5a47c5bad8451667b7a74ad63413dac33b62b88
David <davideliaspc@gmail.com>
2022-12-01 08:55:15 -0600

    [UPDATE] Home screen style and add vector icons.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,24 +1,62 @@
 // Native Libraries
 import React, { useContext } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, Text, TouchableNativeFeedback } from 'react-native';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 // Global Styles
-import { colors } from '../../utilities/styles';
+import { colors, generalStyles } from '../../utilities/styles';
+
+// Icons
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const HomeScreen = (props: any) => {
 
     return (
-        <View style={[ styles.container ]}>
-            {/* Header */}
-            <View style={[ styles.header ]}>
+        <View style={[generalStyles.screen]}>
+            <SafeAreaView style={[ styles.container ]}>
+                <View style={[ styles.container ]}>
+                    {/* Header */}
+                    <View style={[ styles.header ]}>
+                        {/* Title */}
+                        <View style={[ styles.titleContainer ]}>
+                            <Text style={[ styles.title ]}>To-Do App</Text>
+                        </View>
 
-            </View>
+                        {/* Actions */}
+                        <View style={[ styles.actionsContainer ]}>
+                            <View style={[ styles.containerIcon ]}>
+                                <TouchableNativeFeedback>
+                                    <View style={[ styles.containerIcon ]}>
+                                        <Icon name="search" size={20} color="gray" />
+                                    </View>
+                                </TouchableNativeFeedback>
+                            </View>
 
-            {/* Content */}
-            <View style={[ styles.content ]}>
+                            <View style={[ styles.containerIcon ]}>
+                                <TouchableNativeFeedback>
+                                    <View style={[ styles.containerIcon ]}>
+                                        <Icon name="bell-o" size={20} color="gray" />
+                                    </View>
+                                </TouchableNativeFeedback>
+                            </View>
 
-            </View>
+                            <View style={[ styles.containerIcon ]}>
+                                <TouchableNativeFeedback>
+                                    <View style={[ styles.containerIcon ]}>
+                                        <Icon name="bars" size={20} color="gray" />
+                                    </View>
+                                </TouchableNativeFeedback>
+                            </View>
+                        </View>
+                    </View>
+
+                    {/* Content */}
+                    <View style={[ styles.content ]}>
+
+                    </View>
+                </View>
+            </SafeAreaView>
         </View>
     );
 };
@@ -26,18 +64,45 @@ const HomeScreen = (props: any) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        borderWidth: 5,
-        borderColor: 'green'
     },
     header: {
         flex: 1,
-        borderWidth: 5,
-        borderColor: 'blue'
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity:  0.4,
+        shadowRadius: 1,
+        elevation: 5,
     },
     content: {
         flex: 6,
-        borderWidth: 5,
-        borderColor: 'red'
+    },
+    actionsContainer: { 
+        flex: 1, 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'space-around',
+        paddingRight: 10,
+    },
+
+    // Title
+    titleContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        paddingLeft: 30,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: '300'
+    },
+
+    containerIcon: {
+        height: 40,
+        width: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
 
--- src/utilities/styles.ts
+++ src/utilities/styles.ts
@@ -1,6 +1,18 @@
+import { StyleSheet } from "react-native";
+
+// * Colors *
 const white = '#FFF';
 
 export const colors = {
-    // * * * Colors * * *
+    
     background: white,
 };
+
+// * Styles *
+export const generalStyles = StyleSheet.create({
+    screen: { 
+        width: '100%', 
+        height: '100%', 
+        backgroundColor: colors.background,
+    },
+});
